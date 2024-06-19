3ff4249f7b89f925f95c4365b535306c373f36ec
Shahzad <shahzadarain144@gmail.com>
2024-06-19 20:11:37 +0500

    added user schema design

---
+++ src/models/user.model.js
@@ -0,0 +1,37 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"]
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+export const User = mongoose.model("User", userSchema);
